36e87303f607f4e9a2ebccf4d30093c2e8f8d8fd
gopinath <gopinaths2301@gmail.com>
2023-07-13 10:17:44 -0700

    slider outline

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const navToggle = document.querySelector('.mobile-nav-toggle');
 const primaryNav = document.querySelector('.primary-navigation');
 const primaryHeader = document.querySelector('.primary-header');
+const slider = new A11YSlider(document.querySelector('.slider'), {
+    adaptiveHeight: true,
+    dots: false
+});
 navToggle.addEventListener('click', () => {
     primaryNav.hasAttribute('data-visible') ? navToggle.setAttribute("aria-expanded", false) : navToggle.setAttribute("aria-expanded", true);
     primaryNav.toggleAttribute('data-visible');

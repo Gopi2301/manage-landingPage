2ee416fdbd6e93c0ddf44246d8b131d3913619f9
gopinath <gopinaths2301@gmail.com>
2023-07-14 19:08:22 +0530

    slider finish

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ const primaryNav = document.querySelector('.primary-navigation');
 const primaryHeader = document.querySelector('.primary-header');
 const slider = new A11YSlider(document.querySelector('.slider'), {
     adaptiveHeight: true,
-    dots: false
+    dots: true,
+    centerMode: true,
+    arrows: false,
+    responsive: {
+        480: {
+            dots: false,
+            arrows: true
+        }
+    }
 });
 navToggle.addEventListener('click', () => {
     primaryNav.hasAttribute('data-visible') ? navToggle.setAttribute("aria-expanded", false) : navToggle.setAttribute("aria-expanded", true);
